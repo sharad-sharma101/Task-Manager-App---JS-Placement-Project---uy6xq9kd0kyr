b7e281ee964a60d430fec09bb67633897a266601
Sharad Sharma <106671710+sharad-sharma101@users.noreply.github.com>
2023-01-03 19:17:37 +0530

    update css

--- App.js
+++ App.js
@@ -113,8 +113,13 @@ class UI{
         const tableRow = document.createElement('div');
         tableRow.setAttribute('data-id', address.id);
         tableRow.innerHTML = `
-        <div class="note" id="note" data-id=${address.id} draggable="true" >
-         <p><span class = "addressing-name">${address.addrName}</span></p>
+        <div id="note-wrapper">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dot" viewBox="0 0 16 16">
+            <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+        </svg>
+        <div class="note" id="note" data-id=${address.id} draggable="true" > 
+        <p><span class = "addressing-name">${address.addrName}</span></p>
+       </div>
        </div>
         `;
         if(address.labels === "start"){
